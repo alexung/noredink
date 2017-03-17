c78848d54015ce992be25cf22bf0d076f4b2da01
Alex Ung <alexung.ucsb@gmail.com>
2017-03-17 10:56:08 -0700

    set groundwork for writing the assessment and the csv parser

---
+++ begin.rb
@@ -0,0 +1,9 @@
+puts 'You are about to enter the most fulfilling challenge of your life'
+
+puts 'Who are you? '
+name = gets
+
+puts "Get ready to be quizzified, #{name} - How many questions are you up for?: "
+num_questions = gets
+
+Assessment.new(num_questions)
